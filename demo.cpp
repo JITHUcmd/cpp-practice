d0d4a70f566e69da7fc9c3a8ca7211d4a0ed183c
SREEJITH M <sreejithh.manoj@gmail.com>
2025-06-20 20:28:03 +0530

    Initial commit - added basic programs

---
+++ demo.cpp
@@ -0,0 +1,37 @@
+/*#include <iostream>
+using namespace std;
+int main() {
+    int num1, num2;
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+    int sum=num1 + num2;
+    cout <<"SUM:" <<sum <<endl;
+return 0;}*/
+
+/*#include <iostream>
+using namespace std;
+int main() {
+    int a,b,temp;
+    cout<<"enter first number"<<endl;
+    cin>>a;
+    cout<<"enter second number"<<endl;
+    cin>>b;
+    temp=a;
+    a=b;
+    b=temp;
+    cout<<"first number"<<a<<endl;
+    cout<<"second number"<<b<<endl;
+}*/
+
+#include <iostream>
+using namespace std;
+int main() {
+    float r,pi=3.14,area;
+    cout<<"enter the radius of circle"<<endl;
+    cin>>r;
+    area=pi*r*r;
+    cout<<"area of circle:"<<area<<endl;
+
+}
